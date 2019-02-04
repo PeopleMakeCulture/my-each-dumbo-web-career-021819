6c9cebe36245b46e7372b98efb8739035850435c
Jing Cao <jingcao.yale@gmail.com>
2019-02-04 16:58:47 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,9 @@
-def my_each(array)
+def my_each(array) # put argument(s) here
+  # code here
   i = 0
   while i < array.length
     yield array[i]
-    i += i
-  end
+    i+=1
+  end  
 end
 
-collection = [1, 2, 3, 4]
-my_each(collection) do |i|
-  puts i
-end
